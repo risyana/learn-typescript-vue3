b68648f614787cf2faf4c4d03c85e55ac916aae3
Eka Risyana <eka.risyana@mekari.com>
2024-10-02 12:22:06 +0700

    navigation guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import VueCookies from 'vue-cookies'
 import HomeView from '../views/HomeView.vue'
 import SignIn from '../components/SignIn.vue'
 import SignUp from '../components/SignUp.vue'
@@ -9,7 +10,8 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: { requiresAuth: true }, // Set meta field to indicate route needs authentication
     },
     {
       path: '/sign-in',
@@ -31,5 +33,24 @@ const router = createRouter({
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  // Check if the route requires authentication
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    // Check if the authentication cookie exists
+    const authCookie = VueCookies.get('token'); // Replace 'authToken' with your cookie name
+
+    if (authCookie) {
+      // If cookie is present, allow navigation
+      next();
+    } else {
+      // If cookie is not present, redirect to login page
+      next({ name: 'sign-in' });
+    }
+  } else {
+    // If the route doesn't require authentication, allow navigation
+    next();
+  }
+});
 
 export default router
